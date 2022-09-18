4ff21ef29b3d349a9cdb0ca3e68c92b3130d7753
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-09-18 11:05:06 +0500

    tested int & refrence

--- auto/main.cpp
+++ auto/main.cpp
@@ -8,5 +8,15 @@ int main() {
 
 	int i = 2, &r = i;
 	auto a = r;
-	cout << "Hello World! " << i << endl;
+
+	const std::type_info& ti1 = typeid(i);
+	const std::type_info& ti2 = typeid(r);
+
+
+	if (ti1 == ti2) {
+		cout << "are the same" << endl;
+	}
+
+
+
 }
